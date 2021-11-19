768f7a0753448e1e227faf2ad9ec539fbe36a15a
Phi Truong <phihtruong00@gmail.com>
2021-11-18 20:03:35 -0800

    Implemented getQuestions request; partially implemented postQuestion request

--- database/index.js
+++ database/index.js
@@ -4,20 +4,51 @@ mongoose.connect('mongodb://localhost/QA-API')
 .catch(() => console.log('Could not connect'));
 
 let questionSchema = mongoose.Schema({
-  id: String,
-  product_id: String,
+  id: Number,
+  product_id: Number,
   body: String,
-  date_written: String,
+  date_written: Number,
   asker_name: String,
   asker_email: String,
-  reported: String,
-  helpful: String
+  reported: Boolean,
+  helpful: Number
 });
 
-let Question = mongoose.model('Question', questionSchema, 'Questions')
+let Question = mongoose.model('Question', questionSchema, 'questions')
 
 let getOne = () => {
   return Question.findOne({id: '1'}).then(response => response)
 }
+
+module.exports.questionModels = {
+  getQuestions: (productId) => {
+    return Question.find({ product_id: productId })
+      .then(results => results)
+      .catch(err => {
+        console.log('failed to retrieve questions: ', err);
+        return err;
+      });
+  },
+
+  postQuestion: (questionData) => {
+    // TODO: figure out how to implement auto increment of id
+    let newQuestion = new Question(questionData);
+
+    return newQuestion.save()
+      .then(results => results)
+      .catch(err => {
+        console.log('failed to save question: ', err);
+        return err;
+      });
+  },
+
+  put: () => {
+
+  },
+
+  putReported: () => {
+
+  }
+};
 
 module.exports.getOne = getOne;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,54 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 const getOne = require('../database/index.js').getOne
+const { questionModels } = require('../database/index.js')
 
-app.get('/', (req, res) => {
-  getOne().then(response => res.send(response))
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => {
+//   getOne().then(response => res.send(response))
+// })
+
+app.get('/questions/:product_id', (req, res) => {
+  let productId = req.params.product_id;
+
+  return questionModels.getQuestions(productId)
+    .then(results => res.status(200).send(results))
+    .catch(err => res.status(400).send(err));
+})
+
+app.post('/questions/:product_id', (req, res) => {
+  // TODO: figure out how to implement auto increment of id
+  let productId = req.params.product_id;
+  let data = req.body;
+  let createdAt = new Date().getTime();
+
+  return questionModels.postQuestion({
+    id: 3518964,
+    product_id: productId,
+    body: data.body,
+    date_written: createdAt,
+    asker_name: data.name,
+    asker_email: data.email,
+    reported: false,
+    helpful: 0
+  })
+    .then(results => {
+      res.status(201).send(results)
+    })
+    .catch(err => res.status(400).send(err));
+})
+
+app.put('/questions/:question_id/helpful', (req, res) => {
+
+})
+
+app.put('/questions/:question_id/report', (req, res) => {
+
 })
 
 app.listen(port, () => {
